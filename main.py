7fad9f82841a64365d75fface029da161582a3c8
Pedro Vitor <pedro1p0@outlook.com>
2023-12-16 19:34:17 -0300

    feat: uniao de parnamirim e natal, e seus bairros

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ import osmnx as ox
 import pandas as pd
 
 # download and project a street network
-G = ox.graph_from_place('Parnamirim, RN', network_type='drive')
+G = ox.graph_from_place('Cohabinal,Parnamirim, RN', network_type='drive')
 G = ox.project_graph(G)
 fig, ax = ox.plot_graph(G, bgcolor='k', node_size=5, node_color='#999999', node_edgecolor='none', node_zorder=2,
                         edge_color='#555555', edge_linewidth=1.5, edge_alpha=1)
-plt.savefig("graph_original_Parnamirim.png")  # Salvar a primeira imagem
+#plt.savefig("graph_original_Parnamirim.png")  # Salvar a primeira imagem
 
 # node closeness centrality
 node_centrality = nx.closeness_centrality(G)
@@ -21,7 +21,7 @@ df = df.reindex(G.nodes())
 nc = df['colors'].tolist()
 fig, ax = ox.plot_graph(G, bgcolor='k', node_size=10, node_color=nc, node_edgecolor='none', node_zorder=2,
                         edge_color='#555555', edge_linewidth=1.5, edge_alpha=1)
-plt.savefig("graph_nodes_Parnamirim.png")  # Salvar a segunda imagem
+#plt.savefig("graph_nodes_Parnamirim.png")  # Salvar a segunda imagem
 
 # edge closeness centrality: convert graph to a line graph so edges become nodes and vice versa
 edge_centrality = nx.closeness_centrality(nx.line_graph(G))
@@ -36,7 +36,7 @@ ec = [cmap.to_rgba(cl) for cl in ev]
 # color the edges in the original graph with closeness centralities in the line graph
 fig, ax = ox.plot_graph(G, bgcolor='k', ax=None, node_size=0, node_color='w', node_edgecolor='gray', node_zorder=2,
                         edge_color=ec, edge_linewidth=1.5, edge_alpha=1)
-plt.savefig("graph_edges_Parnamirim.png")  # Salvar a terceira imagem
+#plt.savefig("graph_edges_Parnamirim.png")  # Salvar a terceira imagem
 
 # save a shapefile with centrality as node and edge attributes
 nx.set_node_attributes(G, node_centrality, 'node_centrality')
